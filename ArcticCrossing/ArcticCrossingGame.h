567d777966b40e9d04e8d04dcf6e3c9c3ec19f4d
carah101 <carah02@live.co.uk>
2016-11-18 22:59:28 +0000

    Update 33 - Headers

---
+++ ArcticCrossing/ArcticCrossingGame.h
@@ -0,0 +1,12 @@
+#ifndef _ARCTICCROSSINGGAME_H
+#define _ARCTICCROSSINGGAME_H
+
+// Card game headers
+#include "GameConstants.h"
+#include "cSprite.h"
+#include "cBkGround.h"
+#include "cTextureMgr.h"
+#include "cTexture.h"
+#include "cFontMgr.h"
+#include "cSoundMgr.h"
+#endif
